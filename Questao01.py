4a3bcbdc7ae5fbc540c70d1ac91da5040caeb746
Bruno <bsabrunosouza@outlook.com>
2022-03-03 23:04:11 -0300

    criação de sistema de busca

--- Questao01.py
+++ Questao01.py
@@ -10,3 +10,27 @@ def cadastro(n):
                 arquivo.write(f"{nome.capitalize()} {celular}") 
     print("Cadastrado com sucesso")
     return nome_arquivo
+
+def busca(nome_arquivo):
+    try:
+        with open(nome_arquivo,"r",newline="") as arquivo:
+            nome = str(input("Digite o nome de quem você deseja encontrar:"))
+            nome = nome.capitalize()
+            
+            matriz = []
+            for linha in arquivo:
+                linha = linha.rstrip("\n")
+                matriz.append(linha.split(" "))
+
+            achou = False
+            for i in range(len(matriz)):
+                if(matriz[i][0] == nome):
+                    print(f"O numero de {nome} é {matriz[i][1]}")
+                    achou = True
+
+        if(not(achou)):
+            print("Usuário não encontrado, verifique o nome escrito")
+        else:
+            print("Busca realizada com sucesso!")
+    except:
+        print("Arquivo não encontrado, realize o cadastro antes de acessar a busca")
